2c392afb15b143101e7f78185ac63714d938d601
andreaspatakis <BinaryHubTLM123$>
2021-04-19 22:38:37 +0300

    Added comments(In greek)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Η συγκεκριμένη κλάση περιέχει όλες τις χρήσιμες πληροφορίες για τις καταστάσεις οι οποίες μπορούν να προκύψουν στο παιχνίδι.
+# Κάθε αντικείμενο της κλάσης αυτής είναι δηλαδή μια κατάσταση του παιχνιδιού
 class State:
     def __init__(self,left_bank,right_bank,side,prior_move,came_from_obj,depth):
         self.left_bank = left_bank
@@ -8,7 +10,7 @@ class State:
         self.depth = depth
 
 
-#Επιστρέφει τις επιτρεπτές μεταθέσης τις οποίες μπορεί να κάνει η βάρκα
+#Επιστρέφει τις επιτρεπτές μεταθέσεις τις οποίες μπορεί να κάνει η βάρκα
 #στην συγκεκριμένη κατάσταση
 def legalMoves(state_obj,possible_moves):
     moves = []
@@ -29,6 +31,7 @@ def legalMoves(state_obj,possible_moves):
                     moves.append(move)
     return moves
 
+#Ελέγχει οτι καθόλη την διάρκεια των μετακινήσεων, οι Κανίβαλοι και οι Ιεροπόστολοι παραμένουν πάντα 3 και 3
 def permissible(mis,can):
     mis_allowed = False
     can_allowed = False
@@ -38,6 +41,8 @@ def permissible(mis,can):
         can_allowed = True
     return mis_allowed and can_allowed
 
+#Υλοποιεί μια μεταφορά και επιστρέφει το αποτέλεσμα της
+#Δηλαδή πόσοι Κανίβαλοι,Ιεροπόστολοι ειναι αριστερά και δεξία και σε ποίο σημείο βρίσκεται η βάρκα
 def travel(state_obj,move):
     if (state_obj.side == "left"):
         left_miss = state_obj.left_bank[0] - move[0]
@@ -54,6 +59,7 @@ def travel(state_obj,move):
 
     return [[left_miss,left_can],[right_miss,right_can],side]
 
+#Δημιουργεί όλες τις καταστάσεις που μπορούν να προκύψουν απο την τρέχουσα κατάσταση (βάση των κινήσεων οι οποίες επιτρέπεται να γίνουν)
 def createStates(state_obj,moves):
     new_states = []
     for move in moves:
@@ -65,7 +71,7 @@ def createStates(state_obj,moves):
             new_states.append(new_state)
     return new_states
 
-
+#Τυπώνει την όλες τις κινήσεις οι οποίες έγιναν για να οδηγηθούμε στην λύση(ή σε κάποια αλλη κατάσταση)
 def print_journey(state_obj):
     states = []
     state = state_obj
@@ -84,6 +90,7 @@ def print_journey(state_obj):
     if(states[-1].left_bank == [0,0] and states[-1].right_bank == [3,3]):
         print("[0, 0] -- FINAL STATE PROBLEM SOLVED! -- [3, 3]")
 
+#Δέχεται καταστάσεις και επιστρέφει εκείνες οι οποίες αποτελούν λύση(αν υπάρχουν)
 def winning_state(states):
     solutions = []
     for state_obj in states:
@@ -91,23 +98,52 @@ def winning_state(states):
             solutions.append(state_obj)
     return solutions
 
+#Ελέγχει αν ο τύπος δεδομένου ο οποίος δόθηκε αντιστοιχεί σε ακέραιο αριθμό
+def is_int(num):
+    try:
+        value = int(num)
+        return True
+    except ValueError:
+        print("Input must be an integer.")
+        return False
+
+#Ελέγχει αν το περιεομενο της απάντησης είναι το αποδεκτό
+def continue_searching():
+    ans = input("Do you want me to look for another solution?[Y/n]: ")
+    while(ans != "y" and ans != "yes" and ans != "n" and ans != "no"):
+        ans = input("Please type yes or no: ")
+    if(ans == "yes" or ans == "y"):
+        return True
+    else:
+        print("\nByee!")
+        return False
+
+
+# Η υλοποίηση του αλγορίθμου Αναζήτηση Πρώτα σε Πλάτος(Breadth-First Search)
 def bfs(states):
     next_states = []
     while(len(states) != 0):
         state_obj = states[0]
+        if (winning_state([state_obj])):
+            print_journey(state_obj)
+            if(continue_searching() == False):
+                return state_obj
         moves = legalMoves(state_obj,possible_moves)
         new_states = createStates(state_obj,moves)
-        next_states += new_states
+        #next_states += new_states
+        states += new_states
         states.pop(0)
     return next_states
 
+# Η υλοποίηση του αλγορίθμου Επαναληπτικής Εκβάθυνσης(Iterative Deepening)
 def iterative_deepening(states,depth):
     while (len(states) != 0):
         #print("DEPTH: ",states[0].depth)
         state_obj = states[0]
         if (winning_state([state_obj])):
             print_journey(state_obj)
-            return state_obj
+            if(continue_searching() == False):
+                return state_obj
         moves = legalMoves(state_obj, possible_moves)
         new_states = createStates(state_obj, moves)
         states.pop(0)
@@ -118,7 +154,8 @@ def iterative_deepening(states,depth):
                 state_obj = states[0]
                 if(winning_state([state_obj])):
                     print_journey(state_obj)
-                    return state_obj
+                    if(continue_searching() == False):
+                        return state_obj
                 if(state_obj.depth == depth):
                     next_states.append(state_obj)
                     states.pop(0)
@@ -131,23 +168,24 @@ def iterative_deepening(states,depth):
             states = next_states
 
 
-
-
-
-
-
-
-
-def bfs_option(states,max_depth):
-    while (states[0].depth < max_depth):
-        solutions_found = winning_state(states)
-        for state in solutions_found:
-            print_journey(state)
-        if (solutions_found):
-            break
-        else:
-            states = bfs(states)
-
+#Εμφανίζει τα αρχικά μηνύματα στον χρήστη και εκτελεί τον αλγόριθμο αναζήτης τον οποίο θα επιλέξει
+def options_and_exec(states):
+    print("\nHELLO!\nWelcome to Missionairies and Cannibals game solver.\n"+
+    "\nTo find a solution using Breadth-First Search algorithm type 1."+
+    "\nTo find a solution using Iterative Deepening algorithm type 2.")
+    algo = input("\nChoose algorithm: ")
+    while(is_int(algo) == False):
+        algo = input("Choose algorithm: ")
+    algo = int(algo)
+    while(algo != 1 and algo != 2):
+        print("Please choose one of the options 1 and 2\n")
+        algo = int(input("Choose algorithm: "))
+    if(algo == 1):
+        print("\n---Breadth-First Search---\n")
+        bfs(states)
+    else:
+        print("\n---Iterative Deepening---\n")
+        iterative_deepening(states,3)
 
 
 
@@ -159,17 +197,9 @@ if __name__ == '__main__':
 
     # Η κατάσταση κατα την οποία ξεκινάμε (όλοι βρίσκονται στην αριστερά όχθη)
     initial_state = State([3,3],[0,0],"left",[],None,0)
+
     # Η τελική κατάσταση (όλοι βρίσκονται στην δεξία όχθη)
     #goal_state = State([0,0],[3,3],"right")
 
     states = [initial_state]
-    max_depth = 15
-    bfs_option(states,max_depth)
-
-    iterative_deepening(states,3)
-
-
-
-
-
-
+    options_and_exec(states)
